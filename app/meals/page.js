dbbe2ead2d473ae71d34a405d5b0998718bf81ec
vedant.malpani <vedant.malpani@publicissapient.com>
2024-06-10 13:59:02 +0530

    first commit

---
+++ app/meals/page.js
@@ -0,0 +1,36 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import Link from "next/link";
+import { Suspense } from "react";
+import classes from "./page.module.css";
+import MealsLoadingPage from "./loading-out";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favourite recipe</Link>
+        </p>
+      </header>
+
+      <main className={classes.main}>
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Details Not Found</h1>
+      <p>Unfortunatesly we cannot find requested page or resource.</p>
+    </main>
+  );
+}
